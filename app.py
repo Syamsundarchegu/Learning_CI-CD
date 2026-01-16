19b79b625856ed66f4b00b4ad48a14da18f71d48
Syam Sundar Chegu <Syam.sundar@xencia.com>
2026-01-16 14:52:00 +0530

    app.py is updated

--- app.py
+++ app.py
@@ -6,6 +6,6 @@ app = FastAPI()
 
 @app.get('/')
 def get_home():
-    return {"message" : "This is first time i am learning the ci/cd pipelines using the ci/cd"}
+    return {"message" : "This is first time i am learning the ci/cd pipelines using the ci/cd syamsundar"}
 
 
