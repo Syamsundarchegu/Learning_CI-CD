8125aa1350a0a4ac3b1fb67c29206216f4a95f4b
Syam Sundar Chegu <Syam.sundar@xencia.com>
2026-01-16 13:52:40 +0530

    Intial FastAPI Commit

---
+++ app.py
@@ -0,0 +1,11 @@
+from fastapi import FastAPI
+
+
+app = FastAPI()
+
+
+@app.get('/')
+def get_home():
+    return {"message" : "This is first time i am learning the ci/cd pipelines using the ci/cd"}
+
+
